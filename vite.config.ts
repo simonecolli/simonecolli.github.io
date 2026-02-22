f5512dede776386faecffb945e04572485590f61
Simone Colli <s.colli.dev@gmail.com>
2026-02-22 20:50:28 +0100

    Added vitePrerender

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { vitePrerender } from 'vite-plugin-prerender'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    vitePrerender({
+      routes: ['/', '/projects', '/talks', '/blog', '/photography', '/about'],
+    }),
   ],
 })
