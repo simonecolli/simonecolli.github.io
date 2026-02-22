39092a542706ff2138fdb2fa005ba23579fc3eb5
Simone Colli <s.colli.dev@gmail.com>
2026-02-22 22:13:44 +0100

    Removed react helmet for SEO

--- src/prerender.tsx
+++ src/prerender.tsx
@@ -1,19 +1,15 @@
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
 import './i18n'
 import { AppRoutes } from './App.tsx'
 
 export async function prerender(data: { url: string }) {
   const { parseLinks } = await import('vite-prerender-plugin/parse')
-  const helmetContext = {}
 
   const html = renderToString(
-    <HelmetProvider context={helmetContext}>
-      <StaticRouter location={data.url}>
-        <AppRoutes />
-      </StaticRouter>
-    </HelmetProvider>
+    <StaticRouter location={data.url}>
+      <AppRoutes />
+    </StaticRouter>
   )
 
   const links = parseLinks(html)
